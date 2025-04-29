fdb1d4c2e34d27a57321d22d36c68f5a1b18a2f6
Conor Knowles <dev@conorknowles.com>
2025-04-29 23:09:38 +0100

    Add caches

--- nix/modules/nixos/system/nix/default.nix
+++ nix/modules/nixos/system/nix/default.nix
@@ -24,6 +24,17 @@ in
           "@wheel"
           "root"
         ];
+        max-free = mkDefault (3000 * 1024 * 1024);
+        min-free = mkDefault (512 * 1024 * 1024);
+        connect-timeout = 5;
+        substituters = [
+          "https://cache.nixos.org"
+          "https://nix-community.cachix.org"
+        ];
+        trusted-public-keys = [
+          "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+          "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        ];
         auto-optimise-store = lib.mkDefault true;
         use-xdg-base-directories = true;
         experimental-features = [
