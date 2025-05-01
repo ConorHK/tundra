7d6b3251ad256ba17548b13650191150945ee1d0
Conor Knowles <dev@conorknowles.com>
2025-05-01 14:08:33 +0100

    Overlay buggy ISOEnts file

--- flake.nix
+++ flake.nix
@@ -73,5 +73,15 @@
         "x86_64-linux"
         "aarch64-darwin"
       ];
+      nixpkgs.overlays = [
+  (final: prev: {
+    docbook-sgml = prev.docbook-sgml.overrideAttrs (oldAttrs: {
+      src = prev.fetchurl {
+        url = "https://sources.voidlinux.org/ISOEnts-1986/ISOEnts.zip";
+        hash = "sha256-1clrkaqnvc1ja4lj8blr0rdlphngkcda3snm7b9jzvcn76d3br6w";
+      };
+    });
+  })
+  ];
     };
 }
