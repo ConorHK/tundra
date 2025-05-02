e316e1e14bc56f061de74e57c7ebe07cd9602ad2
Conor Knowles <dev@conorknowles.com>
2025-05-02 22:59:41 +0100

    Make greetd fall back to login on logout

--- nix/modules/nixos/desktop/programs/greetd/default.nix
+++ nix/modules/nixos/desktop/programs/greetd/default.nix
@@ -1,11 +1,13 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 with lib;
 let
   cfg = config.desktop.programs.greetd;
+  tuigreet = "${pkgs.greetd.tuigreet}/bin/tuigreet";
 in
 {
   options.desktop.programs.greetd = {
@@ -14,17 +16,29 @@ in
       type = with types; bool;
       description = "enable login greeter";
     };
+    session = mkOption {
+      default = "${pkgs.hyprland}/bin/Hyprland";
+      type = with types; str;
+      description = "session to use";
+    };
   };
 
   config = mkIf cfg.enable {
+    users = {
+      users.greeter.group = "greeter";
+      groups.greeter = { };
+    };
     services.greetd = {
       enable = true;
-      settings = rec {
+      settings = {
         default_session = {
-          command = "Hyprland &> /dev/null"; # TODO: generalize
+          command = "${tuigreet} --remember --cmd ${cfg.session}";
+          user = "greeter";
+        };
+        initial_session = {
+          command = "${cfg.session}";
           user = config.user.name;
         };
-        initial_session = default_session;
       };
     };
   };
