ddbc5b660ed90fcbec9397027d24e6cc568c35f4
Conor Knowles <dev@conorknowles.com>
2025-05-02 20:00:15 +0100

    Add new fun bindings

--- nix/hosts/laptop/hyprland/default.nix
+++ nix/hosts/laptop/hyprland/default.nix
@@ -7,6 +7,20 @@
 with lib;
 let
   cfg = config.desktop.environment.hyprland;
+  laptop_lid_switch = pkgs.writeShellScriptBin "laptop_lid_switch" ''
+    #!/usr/bin/env bash
+
+    if grep open /proc/acpi/button/lid/LID0/state; then
+      hyprctl keyword monitor "eDP-1, 2256x1504@60, 0x0, 1"
+    else
+      if [[ `hyprctl monitors | grep "Monitor" | wc -l` != 1 ]]; then
+        hyprctl keyword monitor "eDP-1, disable"
+      else
+        systemctl suspend
+      fi
+    fi
+  '';
+
 in
 {
   config = mkIf cfg.enable {
@@ -27,6 +41,10 @@ in
         ",XF86MonBrightnessUp, exec, brightnessctl s 10%+"
         ",XF86MonBrightnessDown, exec, brightnessctl s 10%-"
       ];
+      bindl = [
+        ",switch:Lid Switch, exec, ${laptop_lid_switch}/bin/laptop_lid_switch"
+      ];
+
       gestures = {
         workspace_swipe = true;
         workspace_swipe_invert = false;
--- nix/modules/home/desktop/environments/hyprland/keybinds.nix
+++ nix/modules/home/desktop/environments/hyprland/keybinds.nix
@@ -7,15 +7,57 @@
 with lib;
 let
   cfg = config.desktop.environment.hyprland;
+   resize = pkgs.writeShellScriptBin "resize" ''
+    #!/usr/bin/env bash
+
+    # Initially inspired by https://github.com/exoess
+
+    # Getting some information about the current window
+    # windowinfo=$(hyprctl activewindow) removes the newlines and won't work with grep
+    hyprctl activewindow > /tmp/windowinfo
+    windowinfo=/tmp/windowinfo
+
+    # Run slurp to get position and size
+    if ! slurp=$(slurp); then
+    		exit
+    fi
+
+    # Parse the output
+    pos_x=$(echo $slurp | cut -d " " -f 1 | cut -d , -f 1)
+    pos_y=$(echo $slurp | cut -d " " -f 1 | cut -d , -f 2)
+    size_x=$(echo $slurp | cut -d " " -f 2 | cut -d x -f 1)
+    size_y=$(echo $slurp | cut -d " " -f 2 | cut -d x -f 2)
+
+    # Keep the aspect ratio intact for PiP
+    if grep "title: Picture-in-Picture" $windowinfo; then
+    		old_size=$(grep "size: " $windowinfo | cut -d " " -f 2)
+    		old_size_x=$(echo $old_size | cut -d , -f 1)
+    		old_size_y=$(echo $old_size | cut -d , -f 2)
+
+    		size_x=$(((old_size_x * size_y + old_size_y / 2) / old_size_y))
+    		echo $old_size_x $old_size_y $size_x $size_y
+    fi
+
+    # Resize and move the (now) floating window
+    grep "fullscreen: 1" $windowinfo && hyprctl dispatch fullscreen
+    grep "floating: 0" $windowinfo && hyprctl dispatch togglefloating
+    hyprctl dispatch moveactive exact $pos_x $pos_y
+    hyprctl dispatch resizeactive exact $size_x $size_y
+  '';
+
 in
 {
   config = mkIf cfg.enable {
+    home.packages = with pkgs; [
+      slurp
+    ];
     wayland.windowManager.hyprland.settings = {
       bind = [
         "SUPER, Space, exec, rofi -show drun -mode drun"
         "SUPER, Return, exec, alacritty"
-        "SUPER, x, exec, ${pkgs.hyprlock}/bin/hyprlock"
+        "SUPER, x, exec, wlogout"
         "SUPER_SHIFT, F, exec, firefox"
+        "SUPER, R, exec, ${resize}/bin/resize"
         "SUPER, Q, killactive,"
         "SUPER, F, Fullscreen,0"
         "SUPER, h, movefocus, l"
@@ -55,7 +97,14 @@ in
         "SUPER_SHIFT, 8, movetoworkspacesilent,08"
         "SUPER_SHIFT, 9, movetoworkspacesilent,09"
         "SUPER_SHIFT, 0, movetoworkspacesilent,10"
+        "SUPER,u, togglespecialworkspace"
+        "SUPERSHIFT,u, movetoworkspace,special"
       ];
+      bindm = [
+        "SUPER, mouse:272, movewindow"
+        "SUPER, mouse:273, resizewindow"
+      ];
+
     };
   };
 }
