cd9af47049192c042b02ade60979ee3894de2cdf
Conor Knowles <dev@conorknowles.com>
2025-05-02 19:26:56 +0100

    Move hyprland cache

--- nix/modules/home/desktop/environments/hyprland/default.nix
+++ nix/modules/home/desktop/environments/hyprland/default.nix
@@ -34,12 +34,6 @@ in
     home.packages = with pkgs; [
       feh
     ];
-    nix.settings = {
-      trusted-substituters = [ "https://hyprland.cachix.org" ];
-      trusted-public-keys = [
-        "nix-gaming.cachix.org-1:nbjlureqMbRAxR1gJ/f3hxemL9svXaZF/Ees8vCUUs4="
-      ];
-    };
     desktop.programs = {
       hypridle.enable = true;
       hyprlock.enable = true;
