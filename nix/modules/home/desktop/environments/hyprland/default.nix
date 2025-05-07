f6a9db068a91030fb29da07575575d7f149dda7d
Conor Knowles <dev@conorknowles.com>
2025-05-07 18:49:37 +0100

    Add home cache

--- nix/modules/home/desktop/environments/hyprland/default.nix
+++ nix/modules/home/desktop/environments/hyprland/default.nix
@@ -31,6 +31,12 @@ in
     };
   };
   config = mkIf cfg.enable {
+    nix.settings = {
+      trusted-substituters = [ "https://hyprland.cachix.org" ];
+      trusted-public-keys = [
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+      ];
+    };
     home.packages = with pkgs; [
       feh
     ];
