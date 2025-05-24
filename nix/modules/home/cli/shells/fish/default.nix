151d403ea21a16dc768226e1a889d996788c6ca1
Conor Knowles <dev@conorknowles.com>
2025-05-24 22:04:16 +0100

    Add starship prompt

--- nix/modules/home/cli/shells/fish/default.nix
+++ nix/modules/home/cli/shells/fish/default.nix
@@ -1,7 +1,6 @@
 {
   config,
   lib,
-  pkgs,
   ...
 }:
 with lib;
@@ -25,27 +24,98 @@ in
           fish_greeting = "";
         };
         interactiveShellInit = ''
-            # vim mode
-            fish_vi_key_bindings
-            set fish_cursor_default     block      blink
-            set fish_cursor_insert      line       blink
-            set fish_cursor_replace_one underscore blink
-            set fish_cursor_visual      block
+          # vim mode
+          fish_vi_key_bindings
+          set fish_cursor_default     block      blink
+          set fish_cursor_insert      line       blink
+          set fish_cursor_replace_one underscore blink
+          set fish_cursor_visual      block
 
-            # prompt
-            set --global hydro_symbol_prompt "── ─"
-            set --global hydro_symbol_ahead "↑ "
-            set --global hydro_symbol_behind "↓ "
-            set --global hydro_color_git magenta
-            set --global hydro_color_prompt yellow
+          # prompt
+          set --global hydro_symbol_prompt "── ─"
         '';
-        plugins = [
-          {
-            name = "hydro";
-            inherit (pkgs.fishPlugins.hydro) src;
-          }
-        ];
       };
+      starship = {
+        enable = true;
+        settings = {
+          format = lib.concatStrings [
+            "$directory"
+            "$git_branch"
+            "$git_commit"
+            "$git_state"
+            "$git_metrics"
+            "$git_status"
+            "$fill"
+            "$status"
+            "$cmd_duration"
+            "$all"
+            "$line_break"
+            "$character"
+          ];
+          cmd_duration = {
+            format = " [⏱ $duration]($style) ";
+          };
+          directory = {
+            read_only = " 󰌾";
+          };
+          fill = {
+            symbol = "·";
+            style = "bright-black";
+          };
+          git_branch = {
+            symbol = " ";
+            format = "[$symbol$branch(:$remote_branch)]($style) ";
+          };
+          git_status = {
+            format = "(([$conflicted](bright-red) )([$stashed](bright-green) )([$deleted](bright-red) )([$renamed](bright-yellow) )([$modified](bright-yellow) )([$staged](bright-yellow) )([$untracked](bright-blue) )[$ahead_behind](bright-green) )";
+            conflicted = "=$count";
+            ahead = "⇡$count";
+            behind = "⇣$count";
+            diverged = "⇡$ahead_count ⇣$behind_count";
+            untracked = "?$count";
+            stashed = "*$count";
+            modified = "!$count";
+            staged = "+$count";
+            renamed = "»$count";
+            deleted = "✖$count";
+          };
+          hostname = {
+            ssh_symbol = "";
+            format = "[$ssh_symbol$hostname]($style) ";
+            style = "bold yellow";
+          };
+          kubernetes = {
+            disabled = false;
+            format = "[$symbol$context(/$namespace)]($style) ";
+          };
+          nix_shell = {
+            symbol = " ";
+            format = "[$symbol$name \\($state\\)]($style) ";
+          };
+          nodejs = {
+            format = "[$symbol($version )]($style) ";
+            disabled = true;
+          };
+          python = {
+            symbol = " ";
+            format = "[\${symbol}\${pyenv_prefix}(\${version} )(\($virtualenv\) )]($style) ";
+          };
+          status = {
+            disabled = false;
+            symbol = "✘";
+            format = "[$symbol $status]($style) ";
+          };
+          time = {
+            disabled = false;
+            format = "[$time]($style) ";
+            style = "bright-black";
+          };
+          username = {
+            format = "[\${user}]($style) ";
+          };
+        };
+      };
+
       zsh = {
         enable = true;
         initExtra = ''
