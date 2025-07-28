5cce9703622aacd19fdb4eeb411b6610f2c49dff
Conor Knowles <dev@conorknowles.com>
2025-07-28 23:05:22 +0100

    Try new colorscheme

--- nix/modules/nixos/styles/stylix/default.nix
+++ nix/modules/nixos/styles/stylix/default.nix
@@ -52,24 +52,7 @@ with lib;
       enable = true;
       autoEnable = true;
       enableReleaseChecks = false;
-      base16Scheme = {
-        base00 = "#1c1c1c";
-        base01 = "#262626";
-        base02 = "#626262";
-        base03 = "#878787";
-        base04 = "#dfdfaf";
-        base05 = "#dfdfaf";
-        base06 = "#dfdfaf";
-        base07 = "#dfdfaf";
-        base08 = "#af5f5f";
-        base09 = "#af875f";
-        base0A = "#af875f";
-        base0B = "#87875f";
-        base0C = "#87afaf";
-        base0D = "#878787";
-        base0E = "#af8787";
-        base0F = "#87afaf";
-      };
+      base16Scheme = "${pkgs.base16-schemes}/share/themes/atelier-forest.yaml";
 
       targets.plymouth.enable = false;
 
