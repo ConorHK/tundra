1bfc246faa96de5f220c92149aa28bf5e2d0acc5
Conor Knowles <knoconor@amazon.com>
2025-08-06 14:18:58 +0000

    Remove secret function

--- nix/modules/home/system/agenix/default.nix
+++ nix/modules/home/system/agenix/default.nix
@@ -10,41 +10,6 @@ with lib;
 
 let
   cfg = config.system.agenix;
-  secret = pkgs.writeScriptBin "secret" ''
-    #!/usr/bin/env bash
-
-    SECRETS_DIR="$(git rev-parse --show-toplevel)/secrets"
-
-    case "$1" in
-      "edit")
-        if [ -z "$2" ]; then
-          echo "Usage: agenix-helper edit <secret-name>"
-          echo "Available secrets:"
-          find "$SECRETS_DIR" -name "*.age" -type f | sed "s|$SECRETS_DIR/||" | sed 's|\.age$||'
-          exit 1
-        fi
-        agenix -e "$SECRETS_DIR/$2.age"
-        ;;
-      "rekey")
-        echo "Rekeying all secrets..."
-        agenix -r -i "$SECRETS_DIR/default.nix"
-        ;;
-      "list")
-        echo "Available secrets:"
-        find "$SECRETS_DIR" -name "*.age" -type f | sed "s|$SECRETS_DIR/||" | sed 's|\.age$||'
-        ;;
-      *)
-        echo "Agenix helper for Tundra"
-        echo "Usage: agenix-helper <command>"
-        echo ""
-        echo "Commands:"
-        echo "  edit <secret>  - Edit a secret"
-        echo "  rekey          - Rekey all secrets"
-        echo "  list           - List available secrets"
-        ;;
-    esac
-  '';
-
 in
 {
   options.system.agenix = {
@@ -58,7 +23,6 @@ in
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
       inputs.agenix.packages.${system}.default
-      secret
     ];
   };
 }
