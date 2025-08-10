2cd1acccf4f51bed9460ab37ee3d05b5d50eb844
Conor Knowles <dev@conorknowles.com>
2025-08-10 17:52:44 +0100

    Move secrets directory

--- secrets.nix
+++ secrets.nix
@@ -2,5 +2,5 @@ let
   remote-dev-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM1dqJ15DRGT+4lm/hc/Bc9zm6HLOXcEGOVQ3gaEgcJM";
 in
 {
-  "nix/hosts/secrets/ntfy.age".publicKeys = [ remote-dev-user ];
+  "nix/hosts/__secrets/ntfy.age".publicKeys = [ remote-dev-user ];
 }
